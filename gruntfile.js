ee278a84b88f548bfdd9509aca8ef34ecdb76e4e
MatiasRicci <matricc89@gmail.com>
2022-06-25 13:00:54 -0300

    Maqueta

---
+++ gruntfile.js
@@ -0,0 +1,80 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+
+
+        uglify: {
+            prod:{
+                option:{
+                    mangle: false,
+                    banner: 'Modificado ' + '<%= grunt.template.today("yyyy-mm-dd h:MM:ss") %>'
+                },
+                files:{
+                    'web/js/script.min.js':[
+                        'desarrollo/js/script.js'
+                    ]
+                }
+            }
+        },
+
+
+        'dart-sass': {
+            dev:{
+                options: {
+                    sourceMap: false
+				},
+                files:{
+                    'web/css/estilo.css' : 'desarrollo/sass/main.scss'
+                }
+            },
+            prod:{
+                options: {
+					outputStyle: 'compressed',
+                    sourceMap: false
+				},
+                files:{
+                    'web/css/estilo.css' : 'desarrollo/sass/main.scss',
+                }
+            }
+        },
+
+        watch:{
+            scriptFiles:{
+                files: ['desarrollo/js/script.js'],
+                tasks: ['uglify:prod']
+            },
+            sassFiles:{
+                files: 'desarrollo/sass/{,*/}*.{scss, sass}',
+                tasks: ['dart-sass:dev']
+            }
+        },
+        
+        browserSync:{
+            dev:{
+                bsFiles:{
+                    src:[
+                        'web/js/*.js',
+                        'web/css/*.css',
+                        'web/*.html',
+                        'web/*.php',
+                        'web/*/*.html',
+                        'web/*/*.php'
+                    ]
+                },
+                options:{
+                    watchTask:true,
+                    proxy:'http://127.0.0.1/Proyecto/examen/web'
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-dart-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync');
+
+    grunt.registerTask('default', ['browserSync','dart-sass:dev','watch'])
+    grunt.registerTask('produccion', ['uglify:prod','dart-sass:prod'])
+};
